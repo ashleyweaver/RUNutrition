145002c8b17116018d255ef4323e3ad5ec57e064
Ashley Weaver <aweaver2012@gmail.com>
2014-10-12 03:53:53 -0400

    Calcuation works

--- rucalories.py
+++ rucalories.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
 from pprint import pprint
 
@@ -10,6 +10,16 @@ def readFile():
 	data = json.load(json_data)
 	json_data.close()
 	return render_template('template.html', data=data)
+
+@app.route("/submit", methods=["POST"])
+def calculate():
+	total = 0
+	req = request.form
+	for calories in req.keys():
+		for quantity in req.getlist(calories):
+			total += int(calories)*int(quantity)
+
+	return str(total)
 	
 if __name__ == '__main__':
 	app.debug = True
